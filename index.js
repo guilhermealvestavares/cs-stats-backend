2d5f685725a69c94bab10fcc7c455fe57221c63d
Guilherme Tavares <guilhermetavalves@gmail.com>
2020-01-16 16:06:51 -0300

    criando lógica de receber parametro

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
 // importando os pacotes para uso no arquivo index.js
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cycle = require("cycle");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const axios = require("axios");
 
 // crio um servidor express
 const app = express();
 
 // aplico configurações para dentro do servidor express, adicionando middlewares (body-parser, morgan, cors)
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 // DB local (tempo de execução)
-var data = [];
-axios
-  .get(
-    "https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?key=D1108BDE58274899E11D3C55998E2D60&steamid=76561198145496259&format=json&appid=730"
-  )
-  .then(response => data = (response.data));
-  
+let data;
 
+const getISteamUserStats = async steamId => {
+  let response;
 
+    response = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?key=D1108BDE58274899E11D3C55998E2D60&steamid=${steamId}&format=json&appid=730`)
+    return response;
+};
+
+const ErrorMessage = status => {
+  let message;
+  switch(status){
+    case 403:
+      message = 'Você não tem permissão';
+    break;
+    default:
+      message = "Usuário não existe";
+  }
+  return message;
+}
 
 // criação de rota que será acessada utilizando o método HTTP GET/
 // http://localhost:9000/
-app.get('/', (req, res) => {
-  return res.json({ data });
+app.get("/:id", async (req, res) => {
+  let returnResponse;
+  try{
+    data = await getISteamUserStats(req.params.id);
+    console.log('--------------------- SUCESS ---------------------')
+    returnResponse = data.data;
+    return res.json(cycle.decycle(returnResponse));
+  }
+  catch(err){
+    const {status} = err.response;
+    console.log(status);
+    res.statusCode = status;
+    return res.json({"error": status, "message":ErrorMessage(status) });
+  }
+ 
+
+
 });
 
 // criação de rota que será acessada utilizando o método HTTP POST/
 // http://localhost:9000/add
-app.post('/add', (req, res) => {
+app.post("/add", (req, res) => {
   const result = req.body;
 
   if (!result) {
@@ -45,4 +72,4 @@ app.post('/add', (req, res) => {
 });
 
 // o servidor irá rodar dentro da porta 9000
-app.listen(9000, () => console.log('Express started at http://localhost:9000'));
+app.listen(9000, () => console.log("Express started at http://localhost:9000"));
